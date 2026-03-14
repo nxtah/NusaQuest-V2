9b720b6cce7e9b06a4b7200d401537e4978e2aac
nxtah <rizkynath543@gmail.com>
2026-03-14 12:55:43 +0700

    setup struktur folder

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+import '../../styles/pages/profile.module.css';
+
+export default function ProfilePage() {
+  return (
+    <div className="container">
+      <h1>Profile Page</h1>
+    </div>
+  );
+}
