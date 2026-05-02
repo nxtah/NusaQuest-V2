9f0601cd4fc73ce2779203535859da8f105ec4d0
Icha Marisa Mahmuda <chaloeymae26@gmail.com>
2026-05-02 18:07:36 +0700

    integrasi fitur pause dan komponen SettingButton pada halaman game

--- src/app/(protected)/play/[gameID]/[topicID]/[roomID]/ular-tangga/page.tsx
+++ src/app/(protected)/play/[gameID]/[topicID]/[roomID]/ular-tangga/page.tsx
@@ -1,7 +1,13 @@
+'use client'; 
+
+import { useState } from "react";
 import GameBackground from "../../../../../../../features/game-ular-tangga/components/GameBackground";
 import Board from "../../../../../../../features/game-ular-tangga/components/Board";
+import PauseModal from "../../../../../../../features/game-ular-tangga/components/PauseModal";
+import SettingButton from "../../../../../../../features/game-ular-tangga/components/SettingButton";
 
 export default function Page() {
+    const [isPaused, setIsPaused] = useState(false);
     return (
         <main className="relative min-h-screen w-full overflow-x-hidden">
             {/* Background Image */}
@@ -17,6 +23,22 @@ export default function Page() {
                     </div>
                 </div>
             </div>
+
+            {/* Setting Button */}
+            <SettingButton onClick={() => setIsPaused(true)} />
+        
+            <div className="relative z-10 flex flex-col items-center justify-center min-h-screen p-4">
+                <div className="absolute top-0 left-20 w-1/2 h-full flex items-center justify-center p-8 z-20">
+                    <div className="w-full aspect-square">
+                        <Board />
+                    </div>
+                </div>
+            </div>
+
+            <PauseModal 
+                isOpen={isPaused}               
+                onClose={() => setIsPaused(false)} 
+            />
         </main>
     );
 }
