d6539ac96ceb2b1cd8ae78b93af19c287ec2307d
Kaila Zanita <kailazanita.2005@gmail.com>
2026-07-02 09:56:26 +0700

    menambahkan background game nuca

--- src/app/(protected)/play/[gameID]/[topicID]/[roomID]/nusa-card/page.tsx
+++ src/app/(protected)/play/[gameID]/[topicID]/[roomID]/nusa-card/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import React, { useState } from 'react';
+import { useRouter, useParams } from 'next/navigation';
+import { nuca } from '../../../../../../../assets/images/nuca/cloudinaryAssets';
+import GameBackground from '../../../../../../../features/game-nuca/components/GameBackground';
+import RotateDeviceOverlay from '../../../../../../../components/layout/RotateDeviceOverlay';
+import PauseModal from '../../../../../../../components/layout/PauseModal';
+import SettingButton from '../../../../../../../components/layout/SettingButton';
+
 export default function Page() {
-  return <main>Nusa Card</main>;
+  return (
+    <main className="relative min-h-screen w-full overflow-x-hidden">
+      {/* Overlay untuk Rotasi Perangkat */}
+      <RotateDeviceOverlay />
+
+      {/* Background Image */}
+      <div className="fixed inset-0 -z-10 bg-[#59a87d]">
+        <GameBackground />
+      </div>
+    </main>
+  )
 }
